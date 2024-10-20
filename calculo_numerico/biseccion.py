27a06709a07168995b6e4021ffa78e0428dc1e5b
Ezuchu <jesusmiguelrr17@gmail.com>
2024-10-20 18:49:46 -0400

    Agg Errores unitarios

--- calculo_numerico/biseccion.py
+++ calculo_numerico/biseccion.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from math import *
 
 def biseccion(f,a,b,tol,Ni):
@@ -11,19 +12,23 @@ def biseccion(f,a,b,tol,Ni):
             print('-'*100)
             m_previo = m_actual
             m_actual = (a+b)/2
-            er = abs(((m_actual-m_previo)/m_actual))*100.0
+            er = fabs(((m_actual-m_previo)/m_actual))
 
             print("iter =",i)
             print("a =", a ,"\nb =", b,f'\nm{i}= {m_actual}')
-            print("error =", f'{round(er,6)}%')
+            print("error =", f'{round(er*100,6)}%')
 
             if(f(m_actual)*f(b)<0):
                 a = m_actual
-            else:
+            elif(f(m_actual)*f(a)<0):
                 b = m_actual
+            else:
+                return m_actual,er
             
             i = i+1
         print('-'*100)
+    
+    return m_actual,er
     
             
     
@@ -33,7 +38,7 @@ def biseccion(f,a,b,tol,Ni):
 if __name__ == '__main__':
     def f(x):
         return sin(x)-exp(-x)
-    
     a = 0
     b = 1
-    biseccion(f,a,b,3,20)
+    resultado = biseccion(f,a,b,0.03,20)
+    print(f"Raiz: {resultado[0]:0.7f}\nError: {resultado[1]:0.4f}")
